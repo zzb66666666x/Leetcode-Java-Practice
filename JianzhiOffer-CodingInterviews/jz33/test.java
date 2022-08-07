d553830abf62536d332d4944c017722c9e41f183
zzb66666666x <18767066589@163.com>
2022-08-07 17:55:05 +0800

    updated on 2022/8/7

--- JianzhiOffer-CodingInterviews/jz33/test.java
+++ JianzhiOffer-CodingInterviews/jz33/test.java
@@ -1,53 +1,74 @@
+// my code
+// class Solution {
+//     public boolean verifyPostorder(int[] postorder) {
+//         if(postorder == null)
+//             return true;
+//         return check(postorder, 0, postorder.length-1, 0, 0);
+//     }
+
+//     private boolean check(int[] p, int L, int R, int numLimit, int dir){
+//         if (L>=R)
+//             return true;
+//         int rootval = p[R];
+//         int[] ret = search(p, rootval, L, R-1);
+//         if (ret[0] >=0 && ret[1] >=0){
+//             if (ret[0] >= ret[1])
+//                 return false;
+//             if (dir < 0 && p[ret[1]] > numLimit){
+//                 return false;
+//             }
+//             if (dir > 0 && p[ret[0]] < numLimit){
+//                 return false;
+//             }
+//             return check(p, L, ret[0], rootval, -1) && check(p, ret[0]+1, ret[1], rootval, 1);
+//         }else if (ret[0] >= 0){// check left subtree only
+//             if (dir > 0 && p[ret[0]] < numLimit){
+//                 return false;
+//             }
+//             return check(p, L, ret[0], rootval, -1);
+//         }else if (ret[1] >= 0){// check right subtree only
+//             if (dir < 0 && p[ret[1]] > numLimit){
+//                 return false;
+//             }
+//             return check(p, L, ret[1], rootval, 1);
+//         }else{
+//             System.out.println("something wrong");
+//             return false;
+//         }
+//     }
+
+//     private int[] search(int[] nums, int val, int L, int R){
+//         int[] ret = new int[]{-1, -1}; // [0]: smaller than [1]: larger than
+//         for (int i = R; i>=L; i--){
+//             if (ret[0] != -1 && ret[1] != -1)
+//                 break;
+//             if (ret[1] == -1 && nums[i] > val){
+//                 ret[1] = i;
+//             }
+//             if (ret[0] == -1 && nums[i] < val){
+//                 ret[0] = i;
+//             }
+//         }
+//         return ret;
+//     }
+// }
+
+// same algorithm, but cleaner
 class Solution {
     public boolean verifyPostorder(int[] postorder) {
-        if(postorder == null)
-            return true;
-        return check(postorder, 0, postorder.length-1, 0, 0);
+        return recur(postorder, 0, postorder.length - 1);
     }
-
-    private boolean check(int[] p, int L, int R, int numLimit, int dir){
-        if (L>=R)
-            return true;
-        int rootval = p[R];
-        int[] ret = search(p, rootval, L, R-1);
-        if (ret[0] >=0 && ret[1] >=0){
-            if (ret[0] >= ret[1])
-                return false;
-            if (dir < 0 && p[ret[1]] > numLimit){
-                return false;
-            }
-            if (dir > 0 && p[ret[0]] < numLimit){
-                return false;
-            }
-            return check(p, L, ret[0], rootval, -1) && check(p, ret[0]+1, ret[1], rootval, 1);
-        }else if (ret[0] >= 0){// check left subtree only
-            if (dir > 0 && p[ret[0]] < numLimit){
-                return false;
-            }
-            return check(p, L, ret[0], rootval, -1);
-        }else if (ret[1] >= 0){// check right subtree only
-            if (dir < 0 && p[ret[1]] > numLimit){
-                return false;
-            }
-            return check(p, L, ret[1], rootval, 1);
-        }else{
-            System.out.println("something wrong");
-            return false;
-        }
-    }
-
-    private int[] search(int[] nums, int val, int L, int R){
-        int[] ret = new int[]{-1, -1}; // [0]: smaller than [1]: larger than
-        for (int i = R; i>=L; i--){
-            if (ret[0] != -1 && ret[1] != -1)
-                break;
-            if (ret[1] == -1 && nums[i] > val){
-                ret[1] = i;
-            }
-            if (ret[0] == -1 && nums[i] < val){
-                ret[0] = i;
-            }
-        }
-        return ret;
+    boolean recur(int[] postorder, int i, int j) {
+        if(i >= j) return true;
+        int p = i;
+        while(postorder[p] < postorder[j]) p++;
+        int m = p;
+        while(postorder[p] > postorder[j]) p++;
+        return p == j && recur(postorder, i, m - 1) && recur(postorder, m, j - 1);
     }
 }
+
+// 作者：jyd
+// 链接：https://leetcode.cn/problems/er-cha-sou-suo-shu-de-hou-xu-bian-li-xu-lie-lcof/solution/mian-shi-ti-33-er-cha-sou-suo-shu-de-hou-xu-bian-6/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
--- JianzhiOffer-CodingInterviews/jz35/test.java
+++ JianzhiOffer-CodingInterviews/jz35/test.java
@@ -12,49 +12,89 @@ class Node {
     }
 }
 */
-import java.util.HashMap;
 
+// method1: method of hashtable
+// import java.util.HashMap;
+
+// class Solution {
+//     private Node getNode(Node head, int idx){
+//         while (idx>0){
+//             head = head.next;
+//             idx--;
+//         }
+//         return head;
+//     }
+
+//     public Node copyRandomList(Node head) {
+//         if (head == null){
+//             return null;
+//         }
+//         HashMap<Integer, Integer> m = new HashMap<Integer, Integer>();
+//         int idx = 0;
+//         Node temp = head;
+//         Node newlist = null;
+//         Node newhead = null;
+//         Node newprev = null;
+//         while(temp != null){
+//             newlist = new Node(temp.val);
+//             if (newhead == null){
+//                 newhead = newlist;
+//             }
+//             if (newprev!=null){
+//                 newprev.next = newlist;
+//             }
+//             newprev = newlist;
+//             m.put(temp.hashCode(), idx);
+//             idx++;
+//             temp = temp.next;
+//         }
+//         temp = head;
+//         newlist = newhead;
+//         while (temp != null){
+//             if (temp.random != null){
+//                 newlist.random = getNode(newhead, m.get(temp.random.hashCode()));
+//             }
+//             temp = temp.next;
+//             newlist = newlist.next;
+//         }
+//         return newhead;
+//     }
+// }
+
+// method2: split interleaved linked list
 class Solution {
-    private Node getNode(Node head, int idx){
-        while (idx>0){
-            head = head.next;
-            idx--;
-        }
-        return head;
-    }
-
     public Node copyRandomList(Node head) {
-        if (head == null){
-            return null;
+        if(head == null) return null;
+        Node cur = head;
+        // 1. 复制各节点，并构建拼接链表
+        while(cur != null) {
+            Node tmp = new Node(cur.val);
+            tmp.next = cur.next;
+            cur.next = tmp;
+            cur = tmp.next;
         }
-        HashMap<Integer, Integer> m = new HashMap<Integer, Integer>();
-        int idx = 0;
-        Node temp = head;
-        Node newlist = null;
-        Node newhead = null;
-        Node newprev = null;
-        while(temp != null){
-            newlist = new Node(temp.val);
-            if (newhead == null){
-                newhead = newlist;
-            }
-            if (newprev!=null){
-                newprev.next = newlist;
-            }
-            newprev = newlist;
-            m.put(temp.hashCode(), idx);
-            idx++;
-            temp = temp.next;
+        // 2. 构建各新节点的 random 指向
+        cur = head;
+        while(cur != null) {
+            if(cur.random != null)
+                cur.next.random = cur.random.next;
+            cur = cur.next.next;
         }
-        temp = head;
-        newlist = newhead;
-        while (temp != null){
-            if (temp.random != null){
-                newlist.random = getNode(newhead, m.get(temp.random.hashCode()));
-            }
-            temp = temp.next;
-            newlist = newlist.next;
+        // 3. 拆分两链表
+        cur = head.next;
+        Node pre = head, res = head.next;
+        while(cur.next != null) {
+            pre.next = pre.next.next;
+            cur.next = cur.next.next;
+            pre = pre.next;
+            cur = cur.next;
         }
-        return newhead;
+        pre.next = null; // 单独处理原链表尾节点
+        return res;      // 返回新链表头节点
     }
 }
+
+// 作者：jyd
+// 链接：https://leetcode.cn/problems/fu-za-lian-biao-de-fu-zhi-lcof/solution/jian-zhi-offer-35-fu-za-lian-biao-de-fu-zhi-ha-xi-/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
